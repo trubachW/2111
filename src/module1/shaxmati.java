b3afb44a1eecaeee711b9ee85aeaf0e7206d066a
trubachW <video.zakaz.2014@yandex.ru>
2018-10-10 20:41:16 +0300

    111111111

--- src/module1/shaxmati.java
+++ src/module1/shaxmati.java
@@ -1,39 +1,57 @@
-package module1;
-
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
+
 public class shaxmati {
-    public static void main(String[] args) {
-        int a,b,c,d,f,g,i;
-        Scanner scan=new Scanner(System.in);
-        char b1[]={'0','А','Б','В','Г','Д','Е','Ё','Ж'};
-        char b2[]={'0','A','B','C','D','E','F','G','K'};
-        char a1[][]=new char[9][9];
-        f=0;
-        for (i=0; i<9; i++){
-            for (g=0; g<9; g++){
-                if (i==0 && g>=1){
-                    a1[i][g] = b1[f];
+    public static void main(String[] args) throws IOException {
+        char [][] field = new char[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                field[i][j] = '-';
+            }
+        }
+        Scanner in = new Scanner(System.in);
+        int prX = 0;
+        int prY = 1;
+        int prK  = 2;
 
-                }else if (i>=1 && g==0){
-                    a1[i][g] = b2[f];
-                }else if (i>=1 && g>=1){
-                    a1[i][g] = 'x';
+        field[0][1] = 'K';
+        printField(field);
+        while(true) {
+            System.out.println("Ходи!");
+            String s = in.next();
+            int pY = s.charAt(0) - 'A', pX = s.charAt(1) - '1';
+            System.out.println(pX + " " + pY);
+            int g1=prK + ((pX+1)-(prX+1));
+            if(g1 == 5 ) {
+                if (true) {
+                    field[pX][pY] = 'K';
+                    field[prX][prY] = '-';
+                    prX = pX;
+                    prY = pY;
+                    printField(field);
+                } else {
+                    System.out.println("Такой ход невозможен!");
+                    continue;
                 }
+            }
+        }
+    }
 
-                f++;
-                System.out.print(a1[i][g]+" ");
+    static void printField(char [][] a){
+        System.out.print("  ");
+        for (char i = 'A'; i <= 'H'; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(i+1 + " ");
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
             }
             System.out.println();
         }
     }
-
-
 }
- //*if (i==0 && g==1){
-      //  a1[i][g]='z';
-       // }else if (i==0 && g==6) {
-       // a1[i][g]='z';
-       // }else {
-       // a1[i][g] = 'x';
-      //  }
